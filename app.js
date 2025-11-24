11ccf091a59e1a1e046512e48dfd2829bba62bef
AgustinMGauto <agustin_gauto@outlook.com>
2025-11-24 11:22:09 +0000

    agrege extensiones

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extencion = ['.com', '.ar', '.es', '.net']
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let g = 0; g < extencion.length; g++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extencion[g]);
       }
     }
   }
+}
+  
 
